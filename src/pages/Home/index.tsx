993429c31e54870e6ed700b3ff3f2df69100abb1
Felipe Macci <felipemacci@gmail.com>
2022-09-18 14:36:44 -0300

    making section intro: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,57 @@
-import { HomeContainer } from "./styles";
+import { HomeContainer, PresentationItem, PresentationItems } from "./styles";
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import coffee from '../../assets/coffee image.svg'
 
 export function Home() {
   return (
     <HomeContainer className="container">
-      <h1>Home</h1>
+      <section id="intro">
+        <div className="intro__presentation">
+          <h1>Find the perfect coffee for any time of day</h1>
+          <p>With Coffee Delivery you get your coffee wherever you are, anytime</p>
+
+          <PresentationItems>
+            <PresentationItem>
+              <div className="icon">
+                <ShoppingCart size={ 16 } weight="fill" />
+              </div>
+
+              <span className="text">Simple and secure purchase</span>
+            </PresentationItem>
+
+            <PresentationItem>
+              <div className="icon">
+                <Package size={ 16 } weight="fill" />
+              </div>
+              
+              <span className="text">Packaging keeps the coffee intact</span>
+            </PresentationItem>
+
+            <PresentationItem>
+              <div className="icon">
+                <Timer size={ 16 } weight="fill" />
+              </div>
+              
+              <span className="text">Fast and tracked delivery</span>
+            </PresentationItem>
+
+            <PresentationItem>
+              <div className="icon">
+                <Coffee size={ 16 } weight="fill" />
+              </div>
+              
+              <span className="text">Coffee arrives fresh to you</span>
+            </PresentationItem>
+          </PresentationItems>
+        </div>
+        <img className="intro__image" src={ coffee } alt="Cup of coffee" />
+      </section>
+
+      <section id="products">
+        <h2>Our cafes</h2>
+
+        
+      </section>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,76 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 
+  #intro {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    padding: 5.5rem 0;
+  }
+
+  .intro__presentation {
+    h1 {
+      font-family: 'Baloo 2', sans-serif;
+      font-size: 3rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme["base-title"]};
+    }
+
+    p {
+      margin-top: 1rem;
+      line-height: 1.3;
+      font-size: 1.25rem;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+  }
+
+  .intro__image {
+    height: 22.5rem;
+    text-align: right;
+  }
+
+  #products {
+    padding-top: 1.875rem;
+
+    h2 {
+      font-family: 'Baloo 2', sans-serif;
+      font-size: 2rem;
+    }
+  }
+`
+
+export const PresentationItems = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 2.5rem;
+`
+
+export const PresentationItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  width: 50%;
+  margin-top: 1.25rem;
+
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 2rem;
+    height: 2rem;
+    color: ${(props) => props.theme.background};
+    border-radius: 50%;
+  }
+
+  &:nth-of-type(1) .icon { background: ${(props) => props.theme["yellow-dark"]} }
+  &:nth-of-type(2) .icon { background: ${(props) => props.theme["base-text"]} }
+  &:nth-of-type(3) .icon { background: ${(props) => props.theme.yellow} }
+  &:nth-of-type(4) .icon { background: ${(props) => props.theme.purple} }
+
+  .text {
+    color: ${(props) => props.theme["base-text"]};
+  }
 `
