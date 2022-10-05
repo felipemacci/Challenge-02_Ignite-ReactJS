8a9de3f17470ae854bc78ab67200f63de1fdc047
Felipe Macci <felipemacci@gmail.com>
2022-10-05 12:10:53 -0300

    added server

--- src/reducers/CartReducer/reducer.ts
+++ src/reducers/CartReducer/reducer.ts
@@ -1,18 +1,5 @@
 import { ActionTypes } from './actions'
-import espresso from '../../assets/espresso.svg'
-import american from '../../assets/american.svg'
-import creamy from '../../assets/creamy espresso.svg'
-import iced from '../../assets/iced.svg'
-import milk from '../../assets/with milk.svg'
-import latte from '../../assets/latte.svg'
-import cappuccino from '../../assets/cappuccino.svg'
-import macchiato from '../../assets/macchiato.svg'
-import mocchaccino from '../../assets/mochaccino.svg'
-import hot from '../../assets/hot.svg'
-import cuban from '../../assets/cuban.svg'
-import hawaiian from '../../assets/hawaiian.svg'
-import arabic from '../../assets/arabic.svg'
-import irish from '../../assets/irish.svg'
+import { api } from '../../lib/api'
 
 export interface Coffee {
   name: string
@@ -23,135 +10,9 @@ export interface Coffee {
   amount: number
 }
 
-export const coffeeList = [
-  {
-    name: 'Traditional Espresso',
-    description: 'Traditional coffee made with hot water and ground beans',
-    image: espresso,
-    tags: ['Traditional'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'American Espresso',
-    description: 'Diluted espresso, less intense than traditional',
-    image: american,
-    tags: ['Traditional'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Creamy Espresso',
-    description: 'Traditional espresso with creamy foam',
-    image: creamy,
-    tags: ['Traditional'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Iced Espresso',
-    description: 'Drink prepared with espresso coffee and ice cubes',
-    image: iced,
-    tags: ['Traditional', 'Iced'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Coffee with milk',
-    description: 'Half and half traditional espresso with steamed milk',
-    image: milk,
-    tags: ['Traditional', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Latte',
-    description: 'A shot of espresso with twice the milk and creamy foam',
-    image: latte,
-    tags: ['Traditional', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Cappuccino',
-    description:
-      'Cinnamon drink made from equal doses of coffee, milk and foam',
-    image: cappuccino,
-    tags: ['Traditional', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Macchiato',
-    description: 'Espresso mixed with some hot milk and foam',
-    image: macchiato,
-    tags: ['Traditional', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Mochaccino',
-    description: 'Espresso coffee with chocolate syrup, little milk and foam',
-    image: mocchaccino,
-    tags: ['Traditional', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Hot Chocolate',
-    description: 'Drink made with chocolate dissolved in hot milk and coffee',
-    image: hot,
-    tags: ['Special', 'With milk'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Cuban',
-    description: 'Iced espresso drink with rum, cream and mint',
-    image: cuban,
-    tags: ['Special', 'Alcoholic', 'Iced'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Hawaiian',
-    description: 'Sweet drink prepared with coffee and coconut milk',
-    image: hawaiian,
-    tags: ['Special'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Arabic',
-    description: 'Drink prepared with Arabic coffee beans and spices',
-    image: arabic,
-    tags: ['Special'],
-    price: 9.9,
-    amount: 1,
-  },
-
-  {
-    name: 'Irish',
-    description:
-      'Drink made from coffee, Irish whiskey, sugar and whipped cream',
-    image: irish,
-    tags: ['Special', 'Alcoholic'],
-    price: 9.9,
-    amount: 1,
-  },
-]
+export const coffeeList: Coffee[] = await api
+  .get('coffeeList')
+  .then((response) => response.data)
 
 export function CartReducer(state: Coffee[], action: any) {
   switch (action.type) {
